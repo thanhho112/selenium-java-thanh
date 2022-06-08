e6af5f126d6145792c5f81d208cae8bbea99756b
thanhho112 <hntresearch@gmail.com>
2022-06-08 15:47:29 +0700

    Update exercise Lab_04

--- src/lab_04/lab_04.java
+++ src/lab_04/lab_04.java
@@ -4,101 +4,91 @@ import java.security.SecureRandom;
 import java.util.*;
 
 public class lab_04 {
-
     public static void main(String[] args) {
 
         List<Integer> intArray = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
-
         intArray = inputArray();
-
-
-        System.out.println();
-        System.out.println("====MENU======");
-        System.out.print("1. Print all numbers\n" +
-                "2. Print maximum value\n" +
-                "3. Print minimum value\n" +
-                "4. Search number\n");
-
-        System.out.print("Pls input menu with number that you want to choose: ");
-
-        int userInput = scanner.nextInt();
-//
-//        while (userInput < 5){
-//            if (userInput == 1){
-//                printArray(intArray);
-//            }
-//            if(userInput == 2){
-//                maxNumber(intArray);
-//            }
-//            if (userInput == 3){
-//                minValue(intArray);
-//            }
-//            if(userInput == 4){
-//                System.out.println("Pls input number for searching: ");
-//                int number = scanner.nextInt();
-//                searchNumber(number,intArray);
-//            }
-//
-//        }
-        System.out.println("Pls input correct number of menu");
-        printArray(intArray);
-//        maxNumber(intArray);
-//        minValue(intArray);
-        //
+        inputMenu(intArray);
     }
+    private static void inputMenu(List<Integer> intArray) {
 
-    public static void searchNumber(int number, int[] intArray) {
-        for (int elementValue: intArray) {
-            if (number == elementValue)
-                System.out.println("the ");
+        Scanner scanner = new Scanner(System.in);
+        boolean isMenuCorrect = true;
+
+        while (isMenuCorrect) {
+            System.out.println("====MENU======");
+            System.out.print("1. Print all numbers\n" +
+                    "2. Print maximum value\n" +
+                    "3. Print minimum value\n" +
+                    "4. Search number\n");
+
+            System.out.print("Pls input menu number: ");
+            int number = scanner.nextInt();
+            if (number == 1){
+                printArray(intArray);
+
+            } else if (number == 2){
+                maxNumber(intArray);
+
+            } else if (number == 3){
+                minValue(intArray);
+
+            } else if (number == 4){
+                searchNumber(intArray);
+
+            } else {
+                System.out.println("Incorrect menu");
+                isMenuCorrect = false;
+            }
         }
+        System.out.println("Thank you for your choice");
     }
-
-    private static void inputMenu(int[] intArray) {
-    }
-
     public static List<Integer> inputArray() {
         List<Integer> intArrayInput = new ArrayList<>();
-//        int randomNumber = new SecureRandom().nextInt(10);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Pls input number size of Array: ");
         int araySize = scanner.nextInt();
-//        int[] intArrayInput = new int[arayLen];
 
         for (int i = 0; i < araySize; i++) {
-//            System.out.print("Pls input numbers: ");
             intArrayInput.add(new SecureRandom().nextInt(1000));
         }
         printArray(intArrayInput);
         return intArrayInput;
     }
-
     public static void maxNumber(List<Integer> intArr) {
-        int max = 0;
+        int max = intArr.get(0);
         for (int index = 0; index < intArr.size(); index++) {
-            if(max < intArr[index])
-                max = intArr[index];
+            if(max < intArr.get(index))
+                max = intArr.get(index);
         }
         System.out.println("Max Number of Array: " + max);
     }
-
     public static void minValue(List<Integer> intArr){
-        int min = intArr.get[0];
+        int min = intArr.get(0);
         for (int index = 1; index < intArr.size(); index++) {
-            if(min >intArr[index]) {
-                min = intArr[index];
+            if(min >intArr.get(index)) {
+                min = intArr.get(index);
             }
         }
         System.out.println("Min Number of Array: " + min);
+    }
+    public static void searchNumber(List<Integer> intArray) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please input search number: ");
+        int number = scanner.nextInt();
+
+        for (int index = 0; index < intArray.size(); index++) {
+            if (number == intArray.get(index)){
+                System.out.println("Found number= :" + number +" at index = " + index);
+            } else {
+                System.out.println("The number is not existed in Array");
+            }
+        }
     }
     public static void printArray(List<Integer> intArr){
         for (int eleventValue: intArr) {
             System.out.print(eleventValue + " ");
         }
-
     }
-
-
 }
