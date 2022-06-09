02ef7d76c1a8e5636d948727bcf28481462f9265
thanhho112 <hntresearch@gmail.com>
2022-06-09 21:57:54 +0700

    update 6.1

--- src/lab_06/Lab_06.java
+++ src/lab_06/Lab_06.java
@@ -25,8 +25,29 @@ public class Lab_06 {
         * 2. find "hrs" tron string => substring tu index 0-> hrs => get number => parseInt => calculate x 60
         * 3. find "minutes" in string => substring=> get number => parseInt
         * 4. Calculate minutes
+        * Solution 3
+        *   1. split "and" => phan lam 2 doan
+        *   2. roi get number & check unit => calculate
         * */
         String timeStr = "2hrs and 5 minutes";
+        String newNumberStr = null;
+
+        String[] splitStr = timeStr.split("and");
+
+        for (String s:splitStr) {
+            System.out.println(s);
+        }
+
+
+//        for (int i = 0; i < splitStr.length(); i++) {
+//            if (Character.isDigit(timeStr.charAt(i))){
+//                newNumberStr = newNumberStr + timeStr.charAt(i);
+//            }
+//        }
+//        char[] charNumberArr = newNumberStr.toCharArray();
+//        for (char ch: charNumberArr) {
+//            int num1 = ch.pa
+//        }
 
     }
 
