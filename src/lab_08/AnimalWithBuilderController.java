83a197a8c84bd708dd8aebaff34b784644a5a7ba
thanhho112 <hntresearch@gmail.com>
2022-06-21 21:40:36 +0700

    update lab9

--- src/lab_08/AnimalWithBuilderController.java
+++ src/lab_08/AnimalWithBuilderController.java
@@ -10,11 +10,18 @@ public class AnimalWithBuilderController {
 
         // New list animal is created with isFlyable = false;
         List<AnimalWithBuilder> newAnimalList = new ArrayList<>();
+        List<AnimalWithBuilder> newAnimalList_unflyable = new ArrayList<>();
         for (AnimalWithBuilder animal : animalList) {
             if (!animal.isFlyable()) {
                 newAnimalList.add(animal);
+            } else {
+                newAnimalList_unflyable.add(animal);
             }
         }
+        System.out.println("List of register animal for racing: " + newAnimalList.toString());
+        System.out.println("List of audience animal: " + newAnimalList_unflyable.toString());
+
+        // Find winner for the racing
         newAnimalList.sort(Comparator.comparing(AnimalWithBuilder::getSpeed));    // Sort Custom Object by attribute in object
         return newAnimalList.get(newAnimalList.size()-1);
     }
